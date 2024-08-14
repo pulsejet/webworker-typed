aff7955e34098807f9bc74491890f2a3abbdeaaa
ZhangSan <1843894164@qq.com>
2024-08-14 13:01:28 +0800

    Update index.ts
    Increased transfer support.

--- index.ts
+++ index.ts
@@ -2,35 +2,66 @@
  * Data sent from main thread to worker.
  */
 type CommRequest = {
-  reqid: number;
-  name: string;
-  args: any[];
+    reqid: number;
+    name: string;
+    args: unknown[];
 };
 
 /**
  * Data sent from worker to main thread.
  */
 type CommResult = {
-  reqid: number;
-  resolve?: any;
-  reject?: string;
+    reqid: number;
+    resolve?: unknown;
+    reject?: string;
 };
 
 /**
  * Map of function names to functions.
  */
-type FunctionMap = { [name: string]: Function };
+type FunctionMap = { [name: string]: Function; };
 
 /**
  * Utility type to convert all methods in an object to async.
  */
 type Async<T extends FunctionMap> = {
-  [K in keyof T]: T[K] extends (...args: infer A) => Promise<infer R>
+    [K in keyof T]: T[K] extends (...args: infer A) => Promise<infer R>
     ? (...args: A) => Promise<R>
     : T[K] extends (...args: infer A) => infer R
     ? (...args: A) => Promise<R>
     : T[K];
 };
+
+/**
+ * Type to mark an object as transferred.
+ * 
+ * @example
+ * ```ts
+ * // mywoker.ts
+ * function foo(buffer: Transferred<ArrayBuffer>) {
+ *  // buffer is transferred
+ * }
+ */
+export type Transferred<T> = T & { _Is_Transferred_: true; };
+
+/**
+ * Mark an object as transferred.
+ *
+ * @param e Object to transfer
+ * 
+ * @example
+ * ```ts
+ * // main.ts
+ * worker.foo(transfer(new ArrayBuffer(1000)));
+ */
+export function transfer<T extends Object>(e: T): Transferred<T> {
+    if (typeof e === 'object') {
+        // @ts-ignore
+        e._Is_Transferred_ = true;
+        console.log(e);
+    }
+    return e as Transferred<T>;
+}
 
 /**
  * Export methods from a worker to the main thread.
@@ -52,27 +83,27 @@ type Async<T extends FunctionMap> = {
  * ```
  */
 export function exportWorker<T extends FunctionMap>(handlers: T): Async<T> {
-  self.onmessage = async ({ data }: { data: CommRequest }) => {
-    try {
-      // Get handler from registrations
-      const handler = handlers[data.name];
-      if (!handler) throw new Error(`[BUG] No handler for type ${data.name}`);
+    self.onmessage = async ({ data }: { data: CommRequest; }) => {
+        try {
+            // Get handler from registrations
+            const handler = handlers[data.name];
+            if (!handler) throw new Error(`[BUG] No handler for type ${data.name}`);
 
-      // Run handler
-      let result = handler.apply(self, data.args);
-      if (result instanceof Promise) {
-        result = await result;
-      }
+            // Run handler
+            let result = handler.apply(self, data.args);
+            if (result instanceof Promise) {
+                result = await result;
+            }
 
-      // Success - post back to main thread
-      self.postMessage({ reqid: data.reqid, resolve: result } as CommResult);
-    } catch (e: any) {
-      // Error - post back rejection
-      self.postMessage({ reqid: data.reqid, reject: e.message } as CommResult);
-    }
-  };
+            // Success - post back to main thread
+            self.postMessage({ reqid: data.reqid, resolve: result } as CommResult);
+        } catch (e: any) {
+            // Error - post back rejection
+            self.postMessage({ reqid: data.reqid, reject: e.message } as CommResult);
+        }
+    };
 
-  return null as unknown as Async<T>;
+    return null as unknown as Async<T>;
 }
 
 /**
@@ -96,28 +127,28 @@ export function exportWorker<T extends FunctionMap>(handlers: T): Async<T> {
  * ```
  */
 export function importWorker<T>(worker: Worker) {
-  const promises = new Map<number, { resolve: Function; reject: Function }>();
+    const promises = new Map<number, { resolve: Function; reject: Function; }>();
 
-  // Handle messages from worker
-  worker.onmessage = ({ data }: { data: CommResult }) => {
-    const { reqid, resolve, reject } = data;
-    if (resolve) promises.get(reqid)?.resolve(resolve);
-    if (reject) promises.get(reqid)?.reject(reject);
-    promises.delete(reqid);
-  };
+    // Handle messages from worker
+    worker.onmessage = ({ data }: { data: CommResult; }) => {
+        const { reqid, resolve, reject } = data;
+        if (resolve) promises.get(reqid)?.resolve(resolve);
+        if (reject) promises.get(reqid)?.reject(reject);
+        promises.delete(reqid);
+    };
 
-  // Create proxy to call worker methods
-  const proxy = new Proxy(worker, {
-    get(target: Worker, name: string) {
-      return async function wrapper(...args: any[]) {
-        return await new Promise((resolve, reject) => {
-          const reqid = Math.random();
-          promises.set(reqid, { resolve, reject });
-          target.postMessage({ reqid, name, args } as CommRequest);
-        });
-      };
-    },
-  });
+    // Create proxy to call worker methods
+    const proxy = new Proxy(worker, {
+        get(target: Worker, name: string) {
+            return async function wrapper(...args: any[]) {
+                return await new Promise((resolve, reject) => {
+                    const reqid = Math.random();
+                    promises.set(reqid, { resolve, reject });
+                    target.postMessage({ reqid, name, args } as CommRequest, args.filter(e => e?._Is_Transferred_));
+                });
+            };
+        },
+    });
 
-  return proxy as T;
+    return proxy as T;
 }
